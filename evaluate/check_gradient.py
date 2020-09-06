c50ad50d217976745f7c6437116671de78ec62db
dangbb <51513363+dangbb@users.noreply.github.com>
2020-09-06 20:32:32 +0700

    Update check_gradient.py

--- evaluate/check_gradient.py
+++ evaluate/check_gradient.py
@@ -1,7 +1,7 @@
 from linarg_package import *
 import numpy as np
 
-eps = 1e-8
+eps = 1e-6
 
 def check_gradient(output_1, output_2, output_3):
     ### output_1: from evaluate function 1 with x as input
@@ -13,16 +13,34 @@ def check_gradient(output_1, output_2, output_3):
     print("Evaluate derivative function: ", abs((output_2 - output_1) / eps - output_3))
 
 def cost(yhat, input):
-    return L2(yhat, input)
+    return np.sum(np.log(input))
 
 def grad(yhat, input):
-    return L2_derivative(yhat, input)
+    return 1 / input
 
-def check_L2(num_experence):
-    yhat = np.random.normal(size = 10)
+def binary_convterter(yhat):
+    return (yhat > 0) * 1 + (yhat < 0) * 0
+
+def normalize(y):
+    cmin = y.min() - 1e-4
+    cmax = y.max()
+
+    y = (y - cmin) / (cmax - cmin + 1e-4)
+
+    print("After normalize: Max: ", y.max(), " Min: ", y.min())
+    # normalise so that data in range (0.1, 0.9)
+    # make sure when + eps or - eps, the value doesn't exceed the range (0, 1)
+    return y * 0.8 + 0.1
+
+def check_Loss(num_experence):
+    yhat = np.random.rand(10)
 
     for i in range(num_experence):
-        y = np.random.normal(size = 10)
+        y = np.random.rand(10)
+        y = normalize(y)
+
+        print(y)
+
         g = np.zeros(shape = 10)
 
         for j in range(10):
@@ -33,13 +51,24 @@ def check_L2(num_experence):
 
             g[j] = (cost(yhat, y_u) - cost(yhat, y_d)) / (2 * eps) - grad(yhat, y)[j]
 
-        if np.linalg.norm(g) < 1e6:
+            if g[j] > 1e-6:
+                print("Pos: ", j)
+                print("Upper input: ", y_u)
+                print("Lower_input: ", y_d)
+                print("Upper_cost: ", cost(yhat, y_u))
+                print("Lower_cost: ", cost(yhat, y_d))
+                print("Grad: ", grad(yhat, y))
+
+        print(g.max())
+
+        if np.linalg.norm(g) < 1e-6:
             print("Safe")
         else:
             print("Not Safe")
 
+check_Loss(1)
 
-check_L2(3)
+
 
 
 
